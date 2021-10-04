c64ee5047a594bd58487bb959df2a65ea93feb28
horacioMartinez <hmartinezkeller@gmail.com>
2021-10-04 03:11:11 -0300

    .

--- src/train_alarm.py
+++ src/train_alarm.py
@@ -21,9 +21,97 @@ from grid2op.Reward import RedispReward
 from grid2op.Agent import BaseAgent
 from alarm import Alarm
 
+# Podemos poner como reward cuanto aumenta el rho 7 episodios despues de sonar la alarma.
+# O sera, que tan cerca esta de game over ??
+# Podemos predecir la prob de que un episodio sea el anterior a uno de game over?
 
-def one_alarm_action(timestep, last_alarm_trigger_timestep, env, rho):
+# No scientific notation
+np.set_printoptions(suppress=True)
+
+
+def generate_nn_training_data(episodes_data):
+    # Data is the features (rho, etc)
+    input_data = []
+    labels = []  # Label is if we finish in 11 timesteps or less
+    for episode_data in episodes_data:
+        last_timestep = episode_data[len(episode_data) - 1]["timestep"]
+        for timestep_data in episode_data:
+            if timestep_data["done"]:
+                continue
+            current_timestep = timestep_data["timestep"]
+            does_finish_in_11_or_less_timesteps = (last_timestep - current_timestep) <= 11
+            features = []
+            features.append(timestep_data["features"]["rho"])
+            input_data.append(features)
+            labels.append(does_finish_in_11_or_less_timesteps)
+
+    return input_data, labels
+
+
+def nn_alarm_action(timestep, last_alarm_trigger_timestep, feature_last_timestep_rho, env, features):
     assert timestep >= last_alarm_trigger_timestep
+
+    # feature_actions_rho = features["actions_rho"]
+    feature_rho = features["rho"]
+    feature_line_status = features["line_status"]
+    feature_month = features["month"]
+    feature_day = features["day"]
+    feature_day_of_week = features["day_of_week"]
+    feature_hour_of_day = features["hour_of_day"]
+    feature_minute_of_hour = features["minute_of_hour"]
+    feature_timestep_overflow = features["timestep_overflow"]
+    feature_time_before_cooldown_line = features["time_before_cooldown_line"]
+    feature_time_before_cooldown_sub = features["time_before_cooldown_sub"]
+    features_timesteps_since_last_attack = features["timesteps_since_last_attack"]
+    feature_time_next_maintenance = features["time_next_maintence"]
+    feature_under_attack = features["under_attack"]
+    feature_attack_duration = features["opponent_attack_duration"]
+    feature_timesteps_since_ongoing_attack_started = features["timesteps_since_ongoing_attack_started"]
+
+    if feature_last_timestep_rho is None:
+        feature_rho_difference = np.zeros_like(feature_rho)
+    else:
+        feature_rho_difference = feature_last_timestep_rho - feature_rho
+
+    # Limit timesteps to 10 (because 11 is the max timesteps the alarm can wait to have a positive score)
+    feature_time_before_cooldown_line[feature_time_before_cooldown_line > 10] = 11
+    feature_time_before_cooldown_sub[feature_time_before_cooldown_sub > 10] = 11
+    feature_time_next_maintenance[feature_time_next_maintenance > 10] = 99
+
+    # print("feature_rho_difference:")
+    # print(feature_rho_difference)
+    # print("feature_rho")
+    # print(feature_rho)
+    # print("feature_line_status")
+    # print(feature_line_status)
+    # print("feature_month")
+    # print(feature_month)
+    # print("feature_day")
+    # print(feature_day)
+    # print("feature_day_of_week")
+    # print(feature_day_of_week)
+    # print("feature_hour_of_day")
+    # print(feature_hour_of_day)
+    # print("feature_minute_of_hour")
+    # print(feature_minute_of_hour)
+    # print("feature_timestep_overflow")
+    # print(feature_timestep_overflow)
+    # print("feature_time_before_cooldown_line")
+    # print(feature_time_before_cooldown_line)
+    # print("feature_time_before_cooldown_sub")
+    # print(feature_time_before_cooldown_sub)
+    # print("features_timesteps_since_last_attack")
+    # print(features_timesteps_since_last_attack)
+    # print("feature_time_next_maintenance")
+    # print(feature_time_next_maintenance)
+    # print("feature_under_attack")
+    # print(feature_under_attack)
+    # print("feature_attack_duration")
+    # print(feature_attack_duration)
+    # print("feature_timesteps_since_ongoing_attack_started")
+    # print(feature_timesteps_since_ongoing_attack_started)
+
+    return None
 
 
 def naive_alarm_action(timestep, last_alarm_trigger_timestep, env, rho):
@@ -69,7 +157,7 @@ env.seed(SEED)
 # obs = env.reset()
 env.reset()
 
-EPISODES_DATA_PATH = "data/episodes_data_rho_0.8"
+EPISODES_DATA_PATH = "data/episodes_data"
 
 episodes_data_files = [f for f in listdir(EPISODES_DATA_PATH) if isfile(join(EPISODES_DATA_PATH, f))]
 random.shuffle(episodes_data_files)
@@ -118,6 +206,8 @@ print("Filtered out", len(episodes_data) - len(filtered_array), "due to our acti
 episodes_data = filtered_array
 #
 
+RHO_THRESHOLD_VISIT = 0.0
+
 for episode_data in episodes_data:
     episode_name = episodes_data_files[episode_data_index].split("-", 1)[-1].split(".", 1)[0]
     episode_names.append(episode_name)
@@ -139,9 +229,18 @@ for episode_data in episodes_data:
             alarm.update_timestep(timestep, None)
             continue
         assert timestep == episode_data_timestep
-        episode_data_timesteps_record.append(episode_data_timestep)
+
         timestep_data = episode_data[data_index]
+        current_data_index = data_index
         data_index += 1
+
+        #
+        if not timestep_data["done"]:
+            if timestep_data["features"]["rho"].max() < RHO_THRESHOLD_VISIT:
+                alarm.update_timestep(timestep, None)
+                continue
+        #
+        episode_data_timesteps_record.append(episode_data_timestep)
 
         done = timestep_data["done"]
         if done:
@@ -164,6 +263,12 @@ for episode_data in episodes_data:
         # Not done
         features = timestep_data["features"]
 
+        feature_last_timestep_rho = None
+        if current_data_index > 0:
+            last_timestep_data = episode_data[current_data_index - 1]
+            if last_timestep_data["timestep"] == timestep - 1:
+                feature_last_timestep_rho = last_timestep_data["features"]["rho"]
+
         feature_timestep = features["timestep"]
         feature_disc_lines_before_cascade = features["disc_lines_before_cascade"]
         feature_actions_rho = features["actions_rho"]
@@ -185,6 +290,11 @@ for episode_data in episodes_data:
         feature_timestep_overflow = features["timestep_overflow"]
         feature_time_before_cooldown_line = features["time_before_cooldown_line"]
         feature_time_before_cooldown_sub = features["time_before_cooldown_sub"]
+        features_timesteps_since_last_attack = features["timesteps_since_last_attack"]
+        feature_time_next_maintenance = features["time_next_maintence"]
+        feature_under_attack = features["under_attack"]
+        feature_attack_duration = features["opponent_attack_duration"]
+        feature_timesteps_since_ongoing_attack_started = features["timesteps_since_ongoing_attack_started"]
 
         alarm_is_legal = alarm.budget >= alarm.alarm_cost
         valid_actions = []
@@ -197,15 +307,19 @@ for episode_data in episodes_data:
             ]
             # alarm_action = None
             # alarm_action = valid_actions[0]
-            alarm_action = naive_alarm_action(timestep, last_alarm_trigger_timestep, env, feature_rho)
+            # alarm_action = naive_alarm_action(timestep, last_alarm_trigger_timestep, env, feature_rho)
+            alarm_action = nn_alarm_action(
+                timestep, last_alarm_trigger_timestep, feature_last_timestep_rho, env, features
+            )
+            # alarm_action = None
             if alarm_action != None:
                 raise_alarm_vect = alarm_action.raise_alarm
                 last_alarm_trigger_timestep = timestep
 
         alarm.update_timestep(timestep, raise_alarm_vect)
+        # print(feature_rho)
     print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< DONE:", alarm_scores[len(alarm_scores) - 1])
     episode_data_index += 1
-
 
 print("Num episodes:", len(episode_names))
 print("Episodes:", episode_names)
@@ -224,3 +338,7 @@ print(
     "number_of_alarm_failures_due_to_action_leading_to_game_over:",
     number_of_alarm_failures_due_to_action_leading_to_game_over,
 )
+
+training_data = generate_nn_training_data(episodes_data)
+
+print("FINISH!")
